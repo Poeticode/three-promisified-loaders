71be38bda8c9448e32f910e07203fd140e50bbad
Prophesi <silas@level2d.com>
2019-09-06 16:24:05 -0400

    alright one more go

--- _src/app/loaders/get-gltf.js
+++ _src/app/loaders/get-gltf.js
@@ -1,6 +1,10 @@
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { GenericLoader } from '../generic-loader.js';
 
-export function GetGLTF(url, callback) {
+function GetGLTF(url, callback) {
     return GenericLoader(new GLTFLoader(), url, callback);
+}
+
+export {
+    GetGLTF
 }
