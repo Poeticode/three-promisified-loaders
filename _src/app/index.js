fa322860fa11a69a8657194a0f7fbca9d24e0bef
Prophesi <silas@level2d.com>
2019-09-06 15:31:11 -0400

    let's see if this import works

--- _src/app/index.js
+++ _src/app/index.js
@@ -1,3 +1,9 @@
-import loaders from './loaders';
+const files = require.context('./loaders/', false, /\.js$/);
+const loaders = {};
+
+files.keys().forEach((key) => {
+  if (key === './index.js') return;
+  loaders[key.replace(/(\.\/|\.js)/g, '')] = files(key).default;
+});
 
 export default loaders;
