ce949ab0e7b22062817b4d38c396a4bcac373982
Prophesi <silas@level2d.com>
2019-09-06 17:31:48 -0400

    here we go again

--- _src/app/index.js
+++ _src/app/index.js
@@ -1,3 +1,4 @@
-export { GetGLTF } from './loaders/get-gltf.js';
+import { GetGLTF } from './loaders/get-gltf.js';
+export { GetGLTF };
 export { Get3MF } from './loaders/get-3mf.js';
 export { GetAMF } from './loaders/get-amf.js';
