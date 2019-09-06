171e2681b3bc9471e9ffe9473fd1abdd6c65dbd6
Prophesi <silas@level2d.com>
2019-09-06 15:59:16 -0400

    alright this one should work

--- _src/app/index.js
+++ _src/app/index.js
@@ -1,5 +1,3 @@
-import { GetGLTF } from './loaders/get-gltf';
-
-export {
-    GetGLTF
-}
+export { GetGLTF } from './loaders/get-gltf';
+export { Get3MF } from './loaders/get-3mf';
+export { GetAMF } from './loaders/get-amf';
